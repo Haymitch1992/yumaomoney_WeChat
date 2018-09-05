520bd9e081df78d386c05084b90e60a6ccaa48aa
高天阳 <13683265113@163.com>
2018-09-05 09:13:04 +0800

    默认路由图标

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,17 @@
   <div id="app">
     <router-view></router-view>
     <tabbar>
-      <tabbar-item link="Events">
+      <tabbar-item link="Events" :selected="$route.path === '/Events'">
         <img slot="icon" src="./assets/foot-xx.png">
         <img slot="icon-active" src="./assets/foot-xx-active.png">
         <span slot="label">活动</span>
       </tabbar-item>
-      <tabbar-item link="Lending">
+      <tabbar-item link="Lending" :selected="$route.path === '/Lending'">
         <img slot="icon" src="./assets/foot-tz.png">
         <img slot="icon-active" src="./assets/foot-tz-active.png">
         <span slot="label">出借</span>
       </tabbar-item>
-      <tabbar-item link="User">
+      <tabbar-item link="User" :selected="$route.path === '/User'">
         <img slot="icon" src="./assets/foot-wd.png">
         <img slot="icon-active" src="./assets/foot-wd-active.png">
         <span slot="label">我的</span>
