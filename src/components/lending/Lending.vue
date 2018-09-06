5972ca7f990b047364387ad4b54ab1b6e7622b14
高天阳 <13683265113@163.com>
2018-09-06 10:36:21 +0800

    上拉刷新 下拉加载

--- src/components/lending/Lending.vue
+++ src/components/lending/Lending.vue
@@ -3,28 +3,30 @@
     <div class="header-box">
       <x-header :left-options='{showBack: false}' class="header-fixed">出借列表</x-header>
     </div>
-    <swiper loop auto :list="swiper_list" v-model="swiper_index" @on-index-change="swiper_onIndexChange"></swiper>
-    <grid :show-lr-borders="false" >
-      <grid-item label="关于我们" key="0">
-        <img slot="icon" src="../../assets/linkme.png">
-      </grid-item>
-      <grid-item label="在线活动" key="1">
-        <img slot="icon" src="../../assets/appDown.png">
-      </grid-item>
-      <grid-item label="app下载" key="2">
-        <img slot="icon" src="../../assets/active.png">
-      </grid-item>
-      <grid-item label="联系我们" key="3">
-        <img slot="icon" src="../../assets/aboutMe.png">
-      </grid-item>
-    </grid>
     <scroller use-pullup :pullup-config="pullupDefaultConfig" @on-pullup-loading="loadMore"
               use-pulldown :pulldown-config="pulldownDefaultConfig" @on-pulldown-loading="refresh"
               lock-x ref="scrollerBottom" height="-48">
-      <group label-width="4.5em" label-margin-right="2em" label-align="right" v-for="item in list" :key="item.key">
-        <panel :list="item.panel" :type="item.type" @on-img-error="onImgError"></panel>
-        <x-progress :percent="item.progress" :show-cancel="false"></x-progress>
-      </group>
+      <div>
+        <swiper loop auto :list="swiper_list" v-model="swiper_index" @on-index-change="swiper_onIndexChange"></swiper>
+        <grid :show-lr-borders="false" >
+          <grid-item label="关于我们" key="0">
+            <img slot="icon" src="../../assets/linkme.png">
+          </grid-item>
+          <grid-item label="在线活动" key="1">
+            <img slot="icon" src="../../assets/appDown.png">
+          </grid-item>
+          <grid-item label="app下载" key="2">
+            <img slot="icon" src="../../assets/active.png">
+          </grid-item>
+          <grid-item label="联系我们" key="3">
+            <img slot="icon" src="../../assets/aboutMe.png">
+          </grid-item>
+        </grid>
+        <group label-width="4.5em" label-margin-right="2em" label-align="right" v-for="item in list" :key="item.key">
+          <panel :list="item.panel" :type="item.type" @on-img-error="onImgError"></panel>
+          <x-progress :percent="item.progress" :show-cancel="false"></x-progress>
+        </group>
+      </div>
     </scroller>
   </div>
 </template>
@@ -76,7 +78,30 @@
         console.log('refresh')
       },
       loadMore () {
+        var self = this
         console.log('loadMore')
+        self.$refs.scrollerBottom.donePullup()
+        self.list.push(
+          {
+            panel: [
+              {
+                src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+                fallbackSrc: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+                title: 'YM01-B-07' + (10 + self.list.length),
+                desc: '融资额度:100.00万元 期限：2个月',
+                url: '/financeDetail?id=' + (140 + self.list.length),
+                meta: {
+                  source: '年利率',
+                  date: '7% + 5%',
+                  other: '完成比例： 30%'
+                }
+              }
+            ],
+            type: '4',
+            key: self.list.length,
+            progress: 30
+          }
+        )
       }
     },
     data () {
@@ -200,6 +225,48 @@
             type: '4',
             key: '4',
             progress: 100
+          },
+          {
+            panel: [
+              {
+                src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+                title: 'YM01-B-0701',
+                desc: '融资额度:50.00万元 期限：75天',
+                url: {
+                  path: '/financeDetail?id=147',
+                  replace: false
+                },
+                meta: {
+                  source: '年利率',
+                  date: '7% + 3%',
+                  other: '完成比例： 40%'
+                }
+              }
+            ],
+            type: '4',
+            key: '5',
+            progress: 40
+          },
+          {
+            panel: [
+              {
+                src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+                title: 'YM01-A-0901',
+                desc: '融资额度:100.00万元 期限：45天',
+                url: {
+                  path: '/financeDetail?id=146',
+                  replace: false
+                },
+                meta: {
+                  source: '年利率',
+                  date: '7% + 3%',
+                  other: '完成比例： 80%'
+                }
+              }
+            ],
+            type: '4',
+            key: '6',
+            progress: 80
           }
         ],
         swiper_index: 1,
