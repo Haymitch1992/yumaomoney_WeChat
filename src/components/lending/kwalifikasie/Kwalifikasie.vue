4196fdd822c1c6c71d8910fae5b71baa720efc19
高天阳 <13683265113@163.com>
2018-09-21 17:41:29 +0800

    信息披露-企业资质

--- src/components/lending/kwalifikasie/Kwalifikasie.vue
+++ src/components/lending/kwalifikasie/Kwalifikasie.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <x-header>企业资质</x-header>
-    <div v-for="src in list" style="text-align:center;">
-      <span style="font-size:20px;">Loading</span>
-      <x-img :src="src" :webp-src="`${src}?type=webp`" @on-success="success" @on-error="error" class="ximg-demo" error-class="ximg-error" :offset="-100" container="#vux_view_box_body"></x-img>
+    <x-header class="header-fixed">企业资质</x-header>
+    <div style="padding-top: 50px">
+      <div v-for="src in list" style="text-align:center">
+        <span style="font-size:20px;">Loading</span>
+        <x-img :src="src" :webp-src="`${src}?type=webp`" @on-success="success" @on-error="error" class="ximg-demo" error-class="ximg-error" :offset="-100" container="#vux_view_box_body"></x-img>
+      </div>
     </div>
   </div>
 </template>
