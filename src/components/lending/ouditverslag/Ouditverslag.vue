36c5dfb1087dd5f72c377e32cee7221ce0634730
高天阳 <13683265113@163.com>
2018-09-21 17:03:26 +0800

    信息披露-审计报告

--- src/components/lending/ouditverslag/Ouditverslag.vue
+++ src/components/lending/ouditverslag/Ouditverslag.vue
@@ -1,44 +1,87 @@
 <template>
   <div>
     <x-header>审计报告</x-header>
-    <div class="vux-demo">
-      <img class="logo" src="../../../assets/images/logo.png">
+    <div style="margin: 10px;overflow: hidden;" v-for="item in list">
+      <masker style="border-radius: 2px;" :opacity="0">
+        <div class="m-img" :style="{backgroundImage: 'url(' + item.img + ')'}"></div>
+          <div slot="content" class="m-title">
+            <a :href="item.link">{{item.title}}</a>
+          </div>
+      </masker>
     </div>
-    <group title="cell demo">
-      <cell title="第一" value="cool" is-link></cell>
-      <cell title="第二" value="cool" is-link></cell>
-      <cell title="第三" value="cool" is-link></cell>
-    </group>
   </div>
 </template>
 
 <script>
-  import { Group, Cell, XHeader } from 'vux'
+  import { Group, Cell, XHeader, Masker } from 'vux'
 
   export default {
     name: 'Ouditverslag',
     components: {
       Group,
       Cell,
-      XHeader
+      XHeader,
+      Masker
     },
     data () {
       return {
-        // note: changing this line won't causes changes
-        // with hot-reload because the reloaded component
-        // preserves its current state and we are modifying
-        // its initial state.
-        msg: 'Hello World!'
+        list: [
+          {
+            title: '查看详情',
+            img: require('@/assets/images/sjbg-2017.jpeg'),
+            link: 'https://www.yumaomoney.com/integrated/report_2017.pdf'
+          },
+          {
+            title: '查看详情',
+            img: require('@/assets/images/sjbg-2016.jpg'),
+            link: 'https://www.yumaomoney.com/integrated/report_2016.pdf'
+          }, {
+            title: '查看详情',
+            img: require('@/assets/images/sjbg-2015.jpg'),
+            link: 'https://www.yumaomoney.com/integrated/report_2015.pdf'
+          }
+        ]
       }
     }
   }
 </script>
 
 <style>
-  .vux-demo {
-    text-align: center;
+  .m-img {
+    padding-bottom: 65%;
+    display: block;
+    position: relative;
+    max-width: 100%;
+    background-size: cover;
+    background-position: center center;
+    cursor: pointer;
+    border-radius: 2px;
   }
-  .logo {
-    width: 100px;
+
+  .m-title {
+    color: #fff;
+    text-align: center;
+    text-shadow: 0 0 2px rgba(0, 0, 0, .5);
+    font-weight: 500;
+    font-size: 16px;
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 100%;
+    text-align: center;
+    top: 80%;
+    transform: translateY(-50%);
+  }
+
+  .m-title a{
+    color: #fff;
+  }
+
+  .m-time {
+    font-size: 12px;
+    padding-top: 4px;
+    border-top: 1px solid #f0f0f0;
+    display: inline-block;
+    margin-top: 5px;
   }
 </style>
