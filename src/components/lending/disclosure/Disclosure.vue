3a158870c934e53300d019b89f279e5e5b1007ed
高天阳 <13683265113@163.com>
2018-09-21 11:55:58 +0800

    信息披露

--- src/components/lending/disclosure/Disclosure.vue
+++ src/components/lending/disclosure/Disclosure.vue
@@ -1,44 +1,51 @@
 <template>
   <div>
     <x-header>信息披露</x-header>
-    <div class="vux-demo">
-      <img class="logo" src="../../../assets/images/logo.png">
-    </div>
-    <group title="cell demo">
-      <cell title="第一" value="cool" is-link></cell>
-      <cell title="第二" value="cool" is-link></cell>
-      <cell title="第三" value="cool" is-link></cell>
+    <tab>
+      <tab-item selected @on-item-click="listType = 1">基本信息</tab-item>
+      <tab-item @on-item-click="listType = 2">公司架构</tab-item>
+      <tab-item @on-item-click="listType = 3">运营数据</tab-item>
+    </tab>
+    <group v-show="listType === 1">
+      <cell title="工商信息" is-link></cell>
+      <cell title="分支机构" is-link></cell>
+      <cell title="重大事项" is-link></cell>
+      <cell title="审计报告" is-link></cell>
+      <cell title="企业资质" is-link></cell>
+    </group>
+    <group v-show="listType === 2">
+      <cell title="高管团队" is-link></cell>
+      <cell title="组织架构" is-link></cell>
+      <cell title="员工信息" is-link></cell>
+    </group>
+    <group v-show="listType === 3">
+      <cell title="运营数据" is-link></cell>
+      <cell title="运营报告" is-link></cell>
     </group>
   </div>
 </template>
 
 <script>
-  import { Group, Cell, XHeader } from 'vux'
+  import { Group, Cell, XHeader, Tab, TabItem } from 'vux'
 
   export default {
     name: 'Disclosure',
     components: {
       Group,
       Cell,
-      XHeader
+      XHeader,
+      Tab,
+      TabItem
     },
     data () {
       return {
-        // note: changing this line won't causes changes
-        // with hot-reload because the reloaded component
-        // preserves its current state and we are modifying
-        // its initial state.
-        msg: 'Hello World!'
+        listType: 1
       }
+    },
+    methods: {
     }
   }
 </script>
 
 <style>
-  .vux-demo {
-    text-align: center;
-  }
-  .logo {
-    width: 100px;
-  }
 </style>
