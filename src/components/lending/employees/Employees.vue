cf506679d172772449af1bd1240164153d1a0f55
高天阳 <13683265113@163.com>
2018-09-26 11:27:14 +0800

    员工信息分布

--- src/components/lending/employees/Employees.vue
+++ src/components/lending/employees/Employees.vue
@@ -5,7 +5,7 @@
       <v-chart :data="data.age">
         <v-scale y :options="yOptions" />
         <v-tooltip disabled />
-        <v-pie :radius="0.85" series-field="name" />
+        <v-pie :radius="0.85" series-field="name" :colors="['#3aacf2','#86d9f3','#aee7ff','#ccefff']" />
         <v-legend :options="ageOptions" />
       </v-chart>
     </group>
@@ -13,7 +13,7 @@
       <v-chart :data="data.degree">
         <v-scale y :options="yOptions" />
         <v-tooltip disabled />
-        <v-pie :radius="0.85" series-field="name" />
+        <v-pie :radius="0.85" series-field="name" :colors="['#3aacf2','#86d9f3','#aee7ff','#ccefff']" />
         <v-legend :options="degreeOptions" />
       </v-chart>
     </group>
@@ -21,7 +21,7 @@
       <v-chart :data="data.workingLife">
         <v-scale y :options="yOptions" />
         <v-tooltip disabled />
-        <v-pie :radius="0.85" series-field="name" />
+        <v-pie :radius="0.85" series-field="name" :colors="['#3aacf2','#86d9f3','#aee7ff','#ccefff']" />
         <v-legend :options="workingLifeOptions" />
       </v-chart>
     </group>
