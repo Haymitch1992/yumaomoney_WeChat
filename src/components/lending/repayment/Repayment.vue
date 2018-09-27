5277ae66c904efaf91c18c560b079e1896087a8e
高天阳 <13683265113@163.com>
2018-09-27 17:01:18 +0800

    到期还款 加载数据

--- src/components/lending/repayment/Repayment.vue
+++ src/components/lending/repayment/Repayment.vue
@@ -1,30 +1,59 @@
 <template>
   <div>
     <x-header :left-options="leftOptions" @on-click-back="goBack()">到期还款</x-header>
-    <group>
-      <cell primary="title" v-for="item in data" :title="item.title" :value="item.pubdate" :link="item.url" :key="item.id" is-link></cell>
-    </group>
+    <scroller use-pullup :pullup-config="pullupDefaultConfig" @on-pullup-loading="loadMore"
+              lock-x ref="scrollerBottom" height="-48">
+      <group>
+        <cell primary="title" v-for="item in data" :title="item.title" :value="item.pubdate" :link="item.url" :key="item.id" is-link></cell>
+      </group>
+      <divider v-show="(parmes.pageNum === parmes.pageNext)">没有更多了</divider>
+    </scroller>
   </div>
 </template>
 
 <script>
   import _ from 'lodash'
   import $ from 'jquery'
-  import { Group, XHeader, Cell } from 'vux'
+  import { Group, XHeader, Cell, Scroller, Divider } from 'vux'
+
+  const pullupDefaultConfig = {
+    content: '上拉加载更多',
+    pullUpHeight: 60,
+    height: 40,
+    autoRefresh: false,
+    downContent: '释放后加载',
+    upContent: '上拉加载更多',
+    loadingContent: '加载中...',
+    clsPrefix: 'xs-plugin-pullup-'
+  }
 
   export default {
     name: 'SakeInligting',
     components: {
       Group,
       XHeader,
-      Cell
+      Cell,
+      Scroller,
+      Divider
     },
     data () {
       return {
-        data: {},
+        parmes: {
+          type: true,
+          ac: 'list',
+          id: 11,
+          pageSize: 20,
+          pageNum: 1,
+          pageNext: null,
+          pagePre: null,
+          pageTotal: null,
+          total: null
+        },
+        data: [],
         leftOptions: {
           preventGoBack: true
-        }
+        },
+        pullupDefaultConfig: pullupDefaultConfig
       }
     },
     methods: {
@@ -33,19 +62,21 @@
       },
       getList () {
         var self = this
-        self.parmes = {
-          ac: 'list',
-          id: 11,
-          pageSize: 20,
-          pageNum: 1
+        if (!self.parmes.type || (self.parmes.pageNum === self.parmes.pageNext)) {
+          return
         }
-        $.getJSON('https://yumaomoney.com/api/jsonPage.php?ac=list&id=' + 11 + '&pageNum=' + 1 + '&pageSize=' + 20 + '&jsoncallback=?', function (data) {
-          console.log(data)
+        self.parmes.type = false
+        $.getJSON('https://yumaomoney.com/api/jsonPage.php?ac=list&id=' + self.parmes.id + '&pageNum=' + (self.parmes.pageNext ? self.parmes.pageNext : self.parmes.pageNum) + '&pageSize=' + self.parmes.pageSize + '&jsoncallback=?', function (data) {
+          self.parmes.pageNum = data.pageNum
+          self.parmes.pageNext = data.pageNext
+          self.parmes.pagePre = data.pagePre
+          self.parmes.pageTotal = data.pageTotal
+          self.parmes.total = data.total
           _.each(data.data, function (v) {
             v.title = v.title.replace(/^【\d{4}-\d{2}-\d{2}】/g, '')
+            self.data.push(v)
           })
-          self.data = data.data
-          console.log(self.data)
+          self.parmes.type = true
         })
         /**
          * 虽然本地可以请求到数据但部署后跨域
@@ -62,6 +93,14 @@
 //          .catch(function (error) {
 //            console.log(error)
 //          })
+      },
+      /**
+       * 加载更多列表
+       */
+      loadMore () {
+        var self = this
+        self.$refs.scrollerBottom.donePullup()
+        self.getList()
       },
       init () {
         var self = this
