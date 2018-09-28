1ee11547848d56f62264585701c3887ee186904d
高天阳 <13683265113@163.com>
2018-09-28 09:09:17 +0800

    官方公告 加载数据

--- src/components/lending/websiteNotice/WebsiteNotice.vue
+++ src/components/lending/websiteNotice/WebsiteNotice.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <x-header :left-options="leftOptions" @on-click-back="goBack()">官网公告</x-header>
-    <group>
-      <cell primary="title" v-for="item in data" :title="item.title" :value="item.publishTime" :link="'/frontNewsDetails.do?id='+item.id" :key="item.id" is-link></cell>
-    </group>
+    <scroller use-pullup :pullup-config="pullupDefaultConfig" @on-pullup-loading="loadMore"
+              lock-x ref="scrollerBottom" height="-48">
+      <group>
+        <cell primary="title" v-for="item in data" :title="item.title" :value="item.publishTime" :link="'/frontNewsDetails.do?id='+item.id" :key="item.id" is-link></cell>
+      </group>
+      <divider v-show="(parmes.pageNum === parmes.pageTotal)">没有更多了</divider>
+    </scroller>
   </div>
 </template>
 
@@ -11,21 +15,46 @@
   import axios from 'axios'
   import _ from 'lodash'
   import moment from 'moment'
-  import { Group, XHeader, Cell } from 'vux'
+  import { Group, XHeader, Cell, Scroller, Divider } from 'vux'
+
+  const pullupDefaultConfig = {
+    content: '上拉加载更多',
+    pullUpHeight: 60,
+    height: 40,
+    autoRefresh: false,
+    downContent: '释放后加载',
+    upContent: '上拉加载更多',
+    loadingContent: '加载中...',
+    clsPrefix: 'xs-plugin-pullup-'
+  }
 
   export default {
     name: 'WebsiteNotice',
     components: {
       Group,
       XHeader,
-      Cell
+      Cell,
+      Scroller,
+      Divider
     },
     data () {
       return {
+        data: [],
+        parmes: {
+          type: true,
+          ac: 'list',
+          id: 10,
+          pageSize: 20,
+          pageNum: 0,
+          pageNext: null,
+          pagePre: null,
+          pageTotal: 12,
+          total: null
+        },
         leftOptions: {
           preventGoBack: true
         },
-        data: []
+        pullupDefaultConfig: pullupDefaultConfig
       }
     },
     methods: {
@@ -34,21 +63,31 @@
       },
       getList () {
         var self = this
-        self.parmes = {
-          PageNum: 1,
-          PageSize: 10
+        if (!self.parmes.type || (self.parmes.pageNum === self.parmes.pageTotal)) {
+          return
         }
-        axios.get(process.env.BASE_API + '/queryNewsListPage.do', {params: { 'paramMap.PageNum': self.parmes.PageNum, 'paramMap.PageSize': self.parmes.PageSize }})
+        self.parmes.type = false
+        axios.get(process.env.BASE_API + '/queryNewsListPage.do', {params: { 'paramMap.PageNum': self.parmes.pageNum + 1, 'paramMap.PageSize': self.parmes.pageSize }})
           .then(function (res) {
+            self.parmes.pageNum++
             _.each(res.data, function (v) {
               console.log(v)
               v.publishTime = moment(v.publishTime).format('YYYY-MM-DD')
               self.data.push(v)
             })
+            self.parmes.type = true
           })
           .catch(function (error) {
             console.log(error)
           })
+      },
+      /**
+       * 加载更多列表
+       */
+      loadMore () {
+        var self = this
+        self.$refs.scrollerBottom.donePullup()
+        self.getList()
       },
       init () {
         var self = this
