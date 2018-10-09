741df36f27fe605fea74fbecb0bd65c7f8d9f4b2
高天阳 <13683265113@163.com>
2018-10-09 14:57:09 +0800

    个人中心

--- src/components/user/center/Center.vue
+++ src/components/user/center/Center.vue
@@ -1,26 +1,29 @@
 <template>
   <div>
     <x-header>个人中心</x-header>
-    <div class="vux-demo">
-      <img class="logo" src="../../../assets/images/logo.png">
-    </div>
-    <group title="cell demo">
-      <cell title="第一" value="cool" is-link></cell>
-      <cell title="第二" value="cool" is-link></cell>
-      <cell title="第三" value="cool" is-link></cell>
+    <group>
+      <cell title="会员中心" is-link></cell>
+      <cell title="实名认证" value="王**（110*******0016）"></cell>
+      <cell title="手机号" value="136****2672" is-link></cell>
+      <cell title="银行卡" value="已绑定" is-link></cell>
+      <cell title="风险测评" value="保守型" is-link></cell>
     </group>
+    <div class="submit-box">
+      <x-button @click.native="iconType = 'success'" type="primary">安全退出</x-button>
+    </div>
   </div>
 </template>
 
 <script>
-  import { Group, Cell, XHeader } from 'vux'
+  import { Group, Cell, XHeader, XButton } from 'vux'
 
   export default {
     name: 'Center',
     components: {
       Group,
       Cell,
-      XHeader
+      XHeader,
+      XButton
     },
     data () {
       return {
@@ -33,12 +36,3 @@
     }
   }
 </script>
-
-<style>
-  .vux-demo {
-    text-align: center;
-  }
-  .logo {
-    width: 100px;
-  }
-</style>
