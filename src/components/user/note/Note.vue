d2e1564af762f617782bdbae01a0a7566ae4af4c
高天阳 <13683265113@163.com>
2018-10-15 15:50:47 +0800

    资金记录

--- src/components/user/note/Note.vue
+++ src/components/user/note/Note.vue
@@ -2,22 +2,49 @@
   <div>
     <x-header>资金记录</x-header>
     <tab>
-      <tab-item :selected="list.listType === 1" @on-item-click="list.listType = 1">充值记录</tab-item>
-      <tab-item :selected="list.listType === 2" @on-item-click="list.listType = 2">提现记录</tab-item>
-      <tab-item :selected="list.listType === 3" @on-item-click="list.listType = 3">投资记录</tab-item>
-      <tab-item :selected="list.listType === 4" @on-item-click="list.listType = 4">还款记录</tab-item>
-      <tab-item :selected="list.listType === 5" @on-item-click="list.listType = 5">其他</tab-item>
+      <tab-item :selected="tab.tabType === 1" @on-item-click="tab.tabType = 1">充值记录</tab-item>
+      <tab-item :selected="tab.tabType === 2" @on-item-click="tab.tabType = 2">提现记录</tab-item>
+      <tab-item :selected="tab.tabType === 3" @on-item-click="tab.tabType = 3">投资记录</tab-item>
+      <tab-item :selected="tab.tabType === 4" @on-item-click="tab.tabType = 4">还款记录</tab-item>
+      <tab-item :selected="tab.tabType === 5" @on-item-click="tab.tabType = 5">其他</tab-item>
     </tab>
-    <group title="cell demo">
-      <cell title="第一" value="cool" is-link></cell>
-      <cell title="第二" value="cool" is-link></cell>
-      <cell title="第三" value="cool" is-link></cell>
+    <group title="成功充值：123456789.45元" v-if="tab.tabType === 1">
+      <cell title="+10000.00元" inline-desc='2017-12-23 16:00:00' value="网银充值"></cell>
+      <cell title="+20000.00元" inline-desc='2017-12-23 16:00:00' value="快捷充值"></cell>
+      <cell title="+10000.00元" inline-desc='2017-12-23 16:00:00' value="网银充值"></cell>
+    </group>
+    <group title="累计提现成功：123456789.45元" v-if="tab.tabType === 2">
+      <cell title="-10000.00元" inline-desc='2017-12-23 16:00:00' value="T+1"></cell>
+      <cell title="-20000.00元" inline-desc='2017-12-23 16:00:00' value="T+0"></cell>
+      <cell title="-10000.00元" inline-desc='2017-12-23 16:00:00' value="提现失败"></cell>
+    </group>
+    <div v-if="tab.tabType === 3">
+      <group title="累计提现成功：123456789.45元" label-width="4.5em" label-margin-right="2em" label-align="right" v-for="item in list" :key="item.key" v-if="item.index === 0">
+        <panel :list="item.panel" :type="item.type" @on-img-error="onImgError" ></panel>
+      </group>
+      <group label-width="4.5em" label-margin-right="2em" label-align="right" v-for="item in list" :key="item.key" v-if="item.index !== 0">
+        <panel :list="item.panel" :type="item.type" @on-img-error="onImgError"></panel>
+      </group>
+    </div>
+    <group title="已收还款：123456789.45元" v-if="tab.tabType === 4">
+      <cell title="+10000.00元" inline-desc='2017-12-23 16:00:00' value="项目名称 本金"></cell>
+      <cell title="+20000.00元" inline-desc='2017-12-23 16:00:00' value="项目名称 还息3/3"></cell>
+      <cell title="+10000.00元" inline-desc='2017-12-23 16:00:00' value="项目名称 还息3/2"></cell>
+      <cell title="+10000.00元" inline-desc='2017-12-23 16:00:00' value="项目名称 还息3/1"></cell>
+    </group>
+    <group title="累计收益：123456789.45元" v-if="tab.tabType === 5">
+      <cell title="10000.00元" inline-desc='2017-12-23 16:00:00' value="体验金利息"></cell>
+      <cell title="20000.00元" inline-desc='2017-12-23 16:00:00' value="邀请奖励"></cell>
+      <cell title="10000.00元" inline-desc='2017-12-23 16:00:00' value="逾期罚息"></cell>
     </group>
   </div>
 </template>
 
 <script>
-  import { Group, Cell, XHeader, Tab, TabItem } from 'vux'
+  import axios from 'axios'
+  import _ from 'lodash'
+  import moment from 'moment'
+  import { Group, Cell, XHeader, Tab, TabItem, Panel } from 'vux'
 
   export default {
     name: 'Note',
@@ -26,14 +53,58 @@
       Cell,
       XHeader,
       Tab,
-      TabItem
+      TabItem,
+      Panel
     },
     data () {
       return {
-        list: {
-          listType: 1
-        }
+        tab: {
+          tabType: 1
+        },
+        list: []
       }
+    },
+    methods: {
+      onImgError (item, $event) {
+        console.log(item, $event)
+      },
+      /**
+       * 获取列表
+       */
+      getList () {
+        var self = this
+        axios.get(process.env.BASE_API + '/financeJson.do', {params: { 'curPage': self.curPage }})
+          .then(function (res) {
+            _.each(res.data, function (v, k) {
+              var item = {
+                index: k,
+                key: v.id,
+                type: '4',
+                progress: parseInt(v.progress),
+                panel: [
+                  {
+                    title: v.borrowTitle,
+                    desc: '投资金额:' + v.borrowAmount + ' 期限：' + v.deadline,
+                    url: '/user/investment/investmentDetail?id=149',
+                    meta: {
+                      source: '年利率',
+                      date: '7% + ' + (parseInt(v.annualRate) - 7) + '%',
+                      other: '投资时间： ' + moment(v.autoBidEnableTime, 'YY-MM-DD hh:mm:ss').format('YYYY-MM-DD HH:mm:ss')
+                    }
+                  }
+                ]
+              }
+              self.list.push(item)
+            })
+          })
+          .catch(function (error) {
+            console.log(error)
+          })
+      }
+    },
+    created () {
+      var self = this
+      self.getList()
     }
   }
 </script>
