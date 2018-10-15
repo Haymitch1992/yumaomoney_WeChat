ece55623dafbbce632574d8ddd5b9e145dab7eaf
高天阳 <13683265113@163.com>
2018-10-15 10:16:37 +0800

    个人中心 设置 关于

--- src/components/user/about/About.vue
+++ src/components/user/about/About.vue
@@ -6,10 +6,10 @@
       <div>当期版本3.1.0</div>
     </div>
     <group>
-      <cell title="功能介绍" is-link></cell>
-      <cell title="去评分" is-link></cell>
-      <cell title="特别声明" is-link></cell>
-      <cell title="服务协议" is-link></cell>
+      <cell title="功能介绍" is-link link="/user/setting/about/introduced"></cell>
+      <cell title="去评分" is-link link="/user/setting/about/score"></cell>
+      <cell title="特别声明" is-link link="/user/setting/about/statement"></cell>
+      <cell title="服务协议" is-link link="/user/setting/about/agreement"></cell>
     </group>
   </div>
 </template>
