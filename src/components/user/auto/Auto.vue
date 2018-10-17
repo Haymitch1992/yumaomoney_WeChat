724d5f14278008885b984531cbc8bcf50ac47ec8
高天阳 <13683265113@163.com>
2018-10-17 13:43:41 +0800

    自动投标 静态

--- src/components/user/auto/Auto.vue
+++ src/components/user/auto/Auto.vue
@@ -10,13 +10,20 @@
       <selector title="年化收益下限" direction="rtl" :options="earnsList" v-model="data.earnsLower"></selector>
       <selector title="投资期限上限" direction="rtl" :options="termList" v-model="data.termCeiling"></selector>
       <selector title="投资期限下限" direction="rtl" :options="termList" v-model="data.termLower"></selector>
-      <x-input v-model="data.save" type="number" title="保留金额(元)" :is-type="positive" placeholder="请输入保留金额"></x-input>
+      <checklist label-position="left" :options="wayList" v-model="data.wayType"></checklist>
+      <radio :options="saveList" @on-change="saveTypeChange" v-model="data.saveType"></radio>
+      <x-input v-model="data.save" type="number" title="保留金额(元)" :is-type="positive" placeholder="请输入保留金额" :disabled="(data.saveType === '0')"></x-input>
     </group>
+    <div class="pt20 ">
+      <div class="submit-box">
+        <x-button @click.native="iconType = 'success'" type="primary">开启自动投标</x-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import { Group, Cell, XHeader, XInput, Selector, Datetime } from 'vux'
+  import { Group, Cell, XHeader, XInput, Selector, Datetime, Checklist, Radio, XButton } from 'vux'
 
   export default {
     name: 'Auto',
@@ -26,17 +33,22 @@
       XHeader,
       XInput,
       Selector,
-      Datetime
+      Datetime,
+      Checklist,
+      Radio,
+      XButton
     },
     data () {
       return {
         data: {
           amount: null,
-          save: null,
+          save: '',
           earnsLower: '11',
           earnsCeiling: '11',
           termCeiling: '4',
-          termLower: '6'
+          termLower: '6',
+          wayType: [],
+          saveType: '0'
         },
         earnsList: [
           {key: '8', value: '8%'},
@@ -61,12 +73,24 @@
           {key: '11', value: '11月'},
           {key: '12', value: '12月'}
         ],
+        wayList: [ {key: '1', value: '还款方式:按月分析，到期还本'} ],
+        saveList: [
+          {key: '0', value: '无需保留金额'},
+          {key: '1', value: '需保留金额'}
+        ],
         positive: function (value) {
           return {
-            valid: value > 0,
+            valid: value >= 0,
             msg: '单笔金额需大于0'
           }
         }
+      }
+    },
+    methods: {
+      saveTypeChange (val, label) {
+        var self = this
+        self.data.save = ''
+        console.log('change', val, label)
       }
     }
   }
