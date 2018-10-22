8827f118cea1e274989377684ce8649c602af53d
高天阳 <13683265113@163.com>
2018-10-22 11:20:41 +0800

    添加收货人静态页面

--- src/components/user/consignee/Consignee.vue
+++ src/components/user/consignee/Consignee.vue
@@ -1,44 +1,61 @@
 <template>
   <div>
     <x-header>添加收货人</x-header>
-    <div class="vux-demo">
-      <img class="logo" src="../../../assets/images/logo.png">
-    </div>
-    <group title="cell demo">
-      <cell title="第一" value="cool" is-link></cell>
-      <cell title="第二" value="cool" is-link></cell>
-      <cell title="第三" value="cool" is-link></cell>
+    <group>
+      <x-input v-model="data.uName" type="text" :min="1" title="姓名" placeholder="请输入收货人姓名"></x-input>
+      <x-input mask="9999 999 9999" :max="13" v-model="data.phone" keyboard="number" title="手机号码" placeholder="请输入手机号码"></x-input>
+      <x-input v-model="data.postcode" :min="6" :max="6" type="number" title="邮政编码" placeholder="请输入邮政编码"></x-input>
+      <x-input v-model="data.detail" type="text" :min="1" title="详细地址" placeholder="街道、楼牌号等详细地址"></x-input>
     </group>
+    <div class="p15">
+      <check-icon :value.sync="data.default" type="plain">设为默认收货地址</check-icon>
+    </div>
+    <div class="pt20 ">
+      <div class="submit-box">
+        <x-button @click.native="save" type="primary">保存</x-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import { Group, Cell, XHeader } from 'vux'
+  import { Group, Cell, XHeader, XInput, Checklist, CheckIcon, XButton } from 'vux'
 
   export default {
     name: 'Consignee',
     components: {
       Group,
       Cell,
-      XHeader
+      XHeader,
+      XInput,
+      Checklist,
+      CheckIcon,
+      XButton
     },
     data () {
       return {
-        // note: changing this line won't causes changes
-        // with hot-reload because the reloaded component
-        // preserves its current state and we are modifying
-        // its initial state.
-        msg: 'Hello World!'
+        data: {
+          uName: '',
+          phone: '',
+          postcode: '',
+          default: ''
+        },
+        positive: function (value) {
+          return {
+            valid: value >= 0,
+            msg: '单笔金额需大于0'
+          }
+        }
+      }
+    },
+    methods: {
+      save () {
+        var self = this
+        console.log(self.data)
       }
     }
   }
 </script>
 
 <style>
-  .vux-demo {
-    text-align: center;
-  }
-  .logo {
-    width: 100px;
-  }
 </style>
