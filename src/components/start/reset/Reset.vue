02828f388dd41a9490a993ebff2f619d5e294412
高天阳 <13683265113@163.com>
2018-10-23 13:52:17 +0800

    重置密码

--- src/components/start/reset/Reset.vue
+++ src/components/start/reset/Reset.vue
@@ -1,44 +1,81 @@
 <template>
   <div>
     <x-header>重置密码</x-header>
-    <div class="vux-demo">
-      <img class="logo" src="../../../assets/images/logo.png">
-    </div>
-    <group title="cell demo">
-      <cell title="第一" value="cool" is-link></cell>
-      <cell title="第二" value="cool" is-link></cell>
-      <cell title="第三" value="cool" is-link></cell>
+    <group>
+      <x-input v-model="data.newCode" :min="8" :max="16" type="password" title="新密码 "
+               :is-type="positive" ref="refnewCode" @on-change="keyDown('refnewCode')" placeholder="含字母和数字8-16位字符" required></x-input>
+      <x-input v-model="data.newCodeBak" :min="8" :max="16" type="password" title="确认密码"
+               :is-type="positive" ref="refnewCodeBak" @on-change="keyDown('refnewCodeBak')" placeholder="含字母和数字8-16位字符" required></x-input>
     </group>
+    <div class="pt20 ">
+      <div class="submit-box">
+        <x-button @click.native="save" type="primary" :disabled="data.disabled">提交</x-button>
+      </div>
+    </div>
+    <toast v-model="data.toastSame" type="warn" :time="1000" is-show-mask text="新密码与原密码不能相同" position="middle"></toast>
+    <toast v-model="data.toastDifferent" type="warn" :time="1000" is-show-mask text="两次密码不相同" position="middle"></toast>
   </div>
 </template>
 
 <script>
-  import { Group, Cell, XHeader } from 'vux'
+  import { Group, Cell, XHeader, XInput, XButton, Toast } from 'vux'
 
   export default {
     name: 'Reset',
     components: {
       Group,
       Cell,
-      XHeader
+      XHeader,
+      XInput,
+      XButton,
+      Toast
     },
     data () {
       return {
-        // note: changing this line won't causes changes
-        // with hot-reload because the reloaded component
-        // preserves its current state and we are modifying
-        // its initial state.
-        msg: 'Hello World!'
+        data: {
+          newCode: '',
+          newCodeBak: '',
+          disabled: true,
+          toastSame: false,
+          toastDifferent: false
+        },
+        positive: function (value) {
+          if (value.search(/[0-9]/) === -1) {
+            return {
+              msg: '登录密码必须包含数字'
+            }
+          } else if (value.search(/[a-zA-Z]/) === -1) {
+            return {
+              msg: '登录密码必须包含字母'
+            }
+          } else {
+            return {
+              valid: value.length < 16,
+              msg: '登录密码由8-16位字母和数字组成'
+            }
+          }
+        }
+      }
+    },
+    methods: {
+      save () {
+        var self = this
+        if (self.data.newCode !== self.data.newCodeBak) {
+          self.data.toastDifferent = true
+        } else {
+          console.log(self.data)
+        }
+      },
+      keyDown () {
+        var self = this
+        if (self.$refs.refnewCode.valid === true && self.data.newCode !== '' &&
+          self.$refs.refnewCodeBak.valid === true && self.data.newCodeBak !== ''
+        ) {
+          self.data.disabled = false
+        } else {
+          self.data.disabled = true
+        }
       }
     }
   }
 </script>
-
-<style>
-  .vux-demo {
-    text-align: center;
-  }
-  .logo {
-    width: 100px;
-  }
-</style>
