aeb55946542f8c77b6b7738a3536f7c9f5b27c0c
高天阳 <13683265113@163.com>
2018-10-24 15:42:31 +0800

    格式化广东省

--- jsonInit.js
+++ jsonInit.js
@@ -14036,7 +14036,7 @@ var obj = [{
   "regionName": "澳门特别行政区"
 }]
 _.each(obj,function (v,k) {
-  if(v.pcode === '433100'){
+  if(v.pcode === '445300'){
     v['"id"'] = v.regionCode
     v['"title"'] = v.regionName
     // v['"children"'] = []
