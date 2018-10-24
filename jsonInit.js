400d5bb94d7e9d12ccfda68212b62ba23937f60b
高天阳 <13683265113@163.com>
2018-10-24 09:13:13 +0800

    格式化河北省

--- jsonInit.js
+++ jsonInit.js
@@ -14036,7 +14036,7 @@ var obj = [{
   "regionName": "澳门特别行政区"
 }]
 _.each(obj,function (v,k) {
-  if(v.pcode === '130400'){
+  if(v.pcode === '139000'){
     v['"id"'] = v.regionCode
     v['"title"'] = v.regionName
     // v.children = []
