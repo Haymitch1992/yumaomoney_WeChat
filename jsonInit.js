742bf222aa041934fa51b423d1772fe92bbfd587
高天阳 <13683265113@163.com>
2018-10-24 16:38:46 +0800

    格式化云南省

--- jsonInit.js
+++ jsonInit.js
@@ -14036,7 +14036,7 @@ var obj = [{
   "regionName": "澳门特别行政区"
 }]
 _.each(obj,function (v,k) {
-  if(v.pcode === '513400'){
+  if(v.pcode === '533400'){
     v['"id"'] = v.regionCode
     v['"title"'] = v.regionName
     // v['"children"'] = []
