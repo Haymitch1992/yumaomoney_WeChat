5932d5774b5531d20b94949c6815d8f6cb8fc243
高天阳 <13683265113@163.com>
2018-10-24 14:08:45 +0800

    格式化黑龙江省

--- jsonInit.js
+++ jsonInit.js
@@ -14036,7 +14036,7 @@ var obj = [{
   "regionName": "澳门特别行政区"
 }]
 _.each(obj,function (v,k) {
-  if(v.pcode === '222400'){
+  if(v.pcode === '232700'){
     v['"id"'] = v.regionCode
     v['"title"'] = v.regionName
     // v['"children"'] = []
