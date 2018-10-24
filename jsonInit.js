9775d23b530dab600ee7636113239a2a6fcc6c5f
高天阳 <13683265113@163.com>
2018-10-24 15:25:40 +0800

    格式化湖北省

--- jsonInit.js
+++ jsonInit.js
@@ -14036,7 +14036,7 @@ var obj = [{
   "regionName": "澳门特别行政区"
 }]
 _.each(obj,function (v,k) {
-  if(v.pcode === '419000'){
+  if(v.pcode === '429000'){
     v['"id"'] = v.regionCode
     v['"title"'] = v.regionName
     // v['"children"'] = []
