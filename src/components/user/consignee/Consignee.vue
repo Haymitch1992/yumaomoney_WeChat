084f51ea76b148139ccc8c38e5b58b528d5db218
高天阳 <13683265113@163.com>
2018-10-26 13:35:12 +0800

    新建、编辑收货人 级联选择器样式调整

--- src/components/user/consignee/Consignee.vue
+++ src/components/user/consignee/Consignee.vue
@@ -2,10 +2,13 @@
   <div>
     <x-header>添加收货人</x-header>
     <group>
-      <x-input v-model="data.uName" type="text" :min="1" title="姓名" placeholder="请输入收货人姓名"></x-input>
+      <x-input v-model="data.uName" type="text" :min="1" title="姓名  " placeholder="请输入收货人姓名"></x-input>
       <x-input mask="9999 999 9999" :max="13" v-model="data.phone" keyboard="number" title="手机号码" placeholder="请输入手机号码"></x-input>
       <x-input v-model="data.postcode" :min="6" :max="6" type="number" title="邮政编码" placeholder="请输入邮政编码"></x-input>
-      <div id="address">{{data.address}}</div>
+      <div class="cascadeBox">
+        <label style="width: 5em">所在区域</label>
+        <div id="address">{{data.address}}</div>
+      </div>
       <x-input v-model="data.detail" type="text" :min="1" title="详细地址" placeholder="街道、楼牌号等详细地址"></x-input>
     </group>
     <div class="p15">
@@ -41,7 +44,7 @@
           uName: '',
           phone: '',
           postcode: '',
-          address: '所在区域',
+          address: '请选择',
           default: ''
         },
         positive: function (value) {
@@ -91,34 +94,3 @@
     }
   }
 </script>
-
-<style>
-  #address {
-    padding: 10px 15px;
-    position: relative;
-    display: -webkit-box;
-    display: -webkit-flex;
-    display: flex;
-    -webkit-box-align: center;
-    -webkit-align-items: center;
-    align-items: center;
-  }
-  #address::before {
-    content: " ";
-    position: absolute;
-    left: 0;
-    top: 0;
-    right: 0;
-    height: 1px;
-    border-top: 1px solid #D9D9D9;
-    color: #D9D9D9;
-    -webkit-transform-origin: 0 0;
-    transform-origin: 0 0;
-    -webkit-transform: scaleY(0.5);
-    transform: scaleY(0.5);
-    left: 15px;
-  }
-  .mobileSelect .content {
-    text-indent: 0em;
-  }
-</style>
