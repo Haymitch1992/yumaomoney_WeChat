cae3975ca6befcc06c9d9cebdb08696609112ebd
高天阳 <13683265113@163.com>
2018-11-02 11:00:11 +0800

    添加收货人

--- src/components/user/consignee/Consignee.vue
+++ src/components/user/consignee/Consignee.vue
@@ -5,12 +5,8 @@
       <x-input v-model="data.uName" type="text" :min="1" title="姓名  " placeholder="请输入收货人姓名"></x-input>
       <x-input mask="9999 999 9999" :max="13" v-model="data.phone" keyboard="number" title="手机号码" placeholder="请输入手机号码"></x-input>
       <x-input v-model="data.postcode" :min="6" :max="6" type="number" title="邮政编码" placeholder="请输入邮政编码"></x-input>
-      <div class="cascadeBox">
-        <label style="width: 5em">所在区域</label>
-        <div style="width: 75%">
-          <div id="address">{{data.address}}</div>
-        </div>
-      </div>
+      <x-address @on-hide="logHide" @on-show="logShow" title="所在区域" v-model="data.address" :list="data.addressData"
+                 @on-shadow-change="onShadowChange" placeholder="请选择地址" :show.sync="data.showAddress"></x-address>
       <x-input v-model="data.detail" type="text" :min="1" title="详细地址" placeholder="街道、楼牌号等详细地址"></x-input>
     </group>
     <div class="p15">
@@ -25,9 +21,7 @@
 </template>
 
 <script>
-  import { Group, Cell, XHeader, XInput, Checklist, CheckIcon, XButton } from 'vux'
-  import MobileSelect from 'mobile-select'
-  import _ from 'lodash'
+  import { Group, Cell, XHeader, XInput, Checklist, CheckIcon, XButton, XAddress, ChinaAddressV4Data } from 'vux'
 
   export default {
     name: 'Consignee',
@@ -38,7 +32,9 @@
       XInput,
       Checklist,
       CheckIcon,
-      XButton
+      XButton,
+      XAddress,
+      ChinaAddressV4Data
     },
     data () {
       return {
@@ -46,8 +42,12 @@
           uName: '',
           phone: '',
           postcode: '',
-          address: '请选择',
-          default: ''
+          default: true,
+          address: [],
+          detail: '',
+          title: '默认为空',
+          addressData: ChinaAddressV4Data,
+          showAddress: false
         },
         positive: function (value) {
           return {
@@ -61,44 +61,16 @@
       save () {
         var self = this
         console.log(self.data)
+      },
+      onShadowChange (ids, names) {
+        console.log(ids, names)
+      },
+      logHide (str) {
+        console.log('on-hide', str)
+      },
+      logShow () {
+        console.log('on-show')
       }
-    },
-    mounted () {
-      var self = this
-      var mobileSelect = new MobileSelect({
-        trigger: '#address',
-        title: '所在区域',
-        address: '',
-        ensureBtnColor: '#1AAD19',
-        wheels: [
-          {
-            data: self.GLOBAL.addressJson
-          }
-        ],
-        keyMap: {
-          id: 'id',
-          value: 'title',
-          childs: 'children'
-        },
-        callback: function (indexArr, data) {
-          var address = ''
-          _.each(data, function (v, k) {
-            if (k === 0) {
-              address += v.title
-            } else {
-              address += ' ' + v.title
-            }
-          })
-          self.data.address = address
-        }
-      })
-      console.log(mobileSelect)
     }
   }
 </script>
-
-<style>
-  .cascadeSelector {
-    flex: 1;
-  }
-</style>
