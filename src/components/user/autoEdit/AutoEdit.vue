8c4d0fca5751f0e706682307887cd4c833328adb
高天阳 <13683265113@163.com>
2018-11-02 13:08:31 +0800

    自动投标 级联选择器

--- src/components/user/autoEdit/AutoEdit.vue
+++ src/components/user/autoEdit/AutoEdit.vue
@@ -6,18 +6,10 @@
     </group>
     <group>
       <x-input v-model="data.amount" type="number" title="单笔金额(元)" :is-type="positive" placeholder="请输入投资金额"></x-input>
-      <div class="cascadeBox">
-        <label style="width: 8em">年化收益</label>
-        <div style="width: 60%">
-          <div id="earns">{{data.earns}}</div>
-        </div>
-      </div>
-      <div class="cascadeBox">
-        <label style="width: 8em">投资期限</label>
-        <div style="width: 60%">
-          <div id="term">{{data.term}}</div>
-        </div>
-      </div>
+      <x-address @on-hide="logHide" @on-show="logShow" title="年化收益" v-model="data.earns" :list="data.earnsData"
+                 @on-shadow-change="onShadowChange" placeholder="请选择范围" :show.sync="data.showEarns"></x-address>
+      <x-address @on-hide="logHide" @on-show="logShow" title="投资期限" v-model="data.term" :list="data.termData"
+                 @on-shadow-change="onShadowChange" placeholder="请选择范围" :show.sync="data.showTerm"></x-address>
       <checklist label-position="left" :options="wayList" v-model="data.wayType"></checklist>
       <radio :options="saveList" @on-change="saveTypeChange" v-model="data.saveType"></radio>
       <x-input v-model="data.save" type="number" title="保留金额(元)" :is-type="positive" placeholder="请输入保留金额" :disabled="(data.saveType === '0')"></x-input>
@@ -34,7 +26,7 @@
 </template>
 
 <script>
-  import { Group, Cell, XHeader, XInput, Selector, Datetime, Checklist, Radio, XButton, CheckIcon, XSwitch } from 'vux'
+  import { Group, Cell, XHeader, XInput, Selector, Datetime, Checklist, Radio, XButton, CheckIcon, XSwitch, XAddress } from 'vux'
   import MobileSelect from 'mobile-select'
   import _ from 'lodash'
 
@@ -51,19 +43,24 @@
       Radio,
       XButton,
       CheckIcon,
-      XSwitch
+      XSwitch,
+      XAddress
     },
     data () {
       return {
         data: {
           amount: null,
           save: '',
-          earns: '请选择',
-          term: '请选择',
+          earns: [],
+          earnsData: [],
           earnsLower: '',
           earnsCeiling: '',
+          showEarns: false,
+          term: [],
+          termData: [],
           termLower: '',
           termCeiling: '',
+          showTerm: false,
           wayType: [],
           saveType: '0',
           agreement: false,
@@ -91,72 +88,21 @@
       autoCheck () {
         var self = this
         console.log(self.data)
+      },
+      onShadowChange (ids, names) {
+        console.log(ids, names)
+      },
+      logHide (str) {
+        console.log('on-hide', str)
+      },
+      logShow () {
+        console.log('on-show')
       }
     },
     mounted () {
       var self = this
-      var mobileSelectEarns = new MobileSelect({
-        trigger: '#earns',
-        title: '年化收益',
-        ensureBtnColor: '#1AAD19',
-        position: [0, 0, 2],
-        connector: '至',
-        wheels: [
-          {
-            data: self.GLOBAL.earnsJson
-          }
-        ],
-        keyMap: {
-          id: 'id',
-          value: 'title',
-          childs: 'children'
-        },
-        callback: function (indexArr, data) {
-          var earns = ''
-          _.each(data, function (v, k) {
-            if (k === 0) {
-              earns += v.title
-              self.data.earnsLower = v.id
-            } else {
-              earns += '至' + v.title
-              self.data.earnsCeiling = v.id
-            }
-          })
-          self.data.earns = earns
-        }
-      })
-      var mobileSelectTerm = new MobileSelect({
-        trigger: '#term',
-        title: '投资期限',
-        ensureBtnColor: '#1AAD19',
-        position: [0, 0, 2],
-        connector: '至',
-        wheels: [
-          {
-            data: self.GLOBAL.termJson
-          }
-        ],
-        keyMap: {
-          id: 'id',
-          value: 'title',
-          childs: 'children'
-        },
-        callback: function (indexArr, data) {
-          var term = ''
-          _.each(data, function (v, k) {
-            if (k === 0) {
-              term += v.title
-              self.data.termLower = v.id
-            } else {
-              term += '至' + v.title
-              self.data.termCeiling = v.id
-            }
-          })
-          self.data.term = term
-        }
-      })
-      console.log(mobileSelectEarns)
-      console.log(mobileSelectTerm)
+      self.data.earnsData = self.GLOBAL.earnsJson
+      self.data.termData = self.GLOBAL.termJson
     }
   }
 </script>
