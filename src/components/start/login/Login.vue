c68a1afa2fe97e4f12da0208644fbdc58a7c9589
高天阳 <13683265113@163.com>
2018-11-05 14:23:04 +0800

    登录接口

--- src/components/start/login/Login.vue
+++ src/components/start/login/Login.vue
@@ -4,8 +4,12 @@
     <group>
       <x-input :max="11" v-model="data.phone" keyboard="number" title="手机号"
                ref="refPhone" @on-change="keyDown()" laceholder="请输入手机号码"></x-input>
-      <x-input v-model="data.newCode" :min="8" :max="16" type="password" title="密码 "
-               ref="refCode" @on-change="keyDown('refnewCode')" placeholder="含字母和数字8-16位字符" required></x-input>
+      <x-input v-model="data.password" :min="8" :max="16" type="password" title="密码 "
+               ref="refPassword" @on-change="keyDown()" placeholder="含字母和数字8-16位字符" required></x-input>
+      <x-input v-model="data.code" :min="4" :max="4" type="text" title="验证码" ref="refCode"
+               @on-change="keyDown()" placeholder="请输入验证码" required>
+        <img slot="right-full-height" src="https://ws1.sinaimg.cn/large/663d3650gy1fq684go3glj203m01hmwy.jpg">
+      </x-input>
     </group>
     <div class="pt20">
       <div class="submit-box">
@@ -26,6 +30,7 @@
 </template>
 
 <script>
+  import qs from 'qs'
   import { Group, Cell, XHeader, XInput, XButton, Toast } from 'vux'
 
   export default {
@@ -42,6 +47,8 @@
       return {
         data: {
           phone: '',
+          password: '',
+          code: '1234',
           disabled: false,
           toastMsg: false
         }
@@ -53,12 +60,34 @@
         if (self.data.phone.length !== 11) {
           self.data.toastMsg = true
         } else {
-          self.$router.push({name: 'home', params: {data: self.data}})
+//          self.$router.push({name: 'home', params: {data: self.data}})
+//          var params = {
+//            paramMap : {
+//              pageId : 'userlogin',
+//              email : self.data.phone,
+//              password : self.data.password,
+//              code :  self.data.code
+//            }
+//          }
+          var param = {}
+          param['paramMap.pageId'] = 'userlogin'
+          param['paramMap.email'] = self.data.phone
+          param['paramMap.password'] = self.data.password
+          param['paramMap.code'] = self.data.code
+          param['paramMap.afterLoginUrl'] = '{session.afterLoginUrl}'
+          self.$http.post(process.env.BASE_API + '/logining.do', qs.stringify(param))
+            .then(function (res) {
+              self.detail = res.data
+            })
+            .catch(function (error) {
+              console.log(error)
+            })
         }
       },
       keyDown () {
         var self = this
         if (self.$refs.refPhone.valid === true && self.data.phone !== '' &&
+          self.$refs.refPassword.valid === true && self.data.password !== '' &&
           self.$refs.refCode.valid === true && self.data.code !== ''
         ) {
           self.data.disabled = false
