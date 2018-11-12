9d15c77709f1c403fc21c7b9c7ce322f60cedf2b
高天阳 <13683265113@163.com>
2018-11-12 13:47:19 +0800

    体验金 数据渲染

--- src/components/user/experience/Experience.vue
+++ src/components/user/experience/Experience.vue
@@ -8,7 +8,7 @@
     </tab>
     <div v-if="list.listType === 1">
       <group :gutter="37"></group>
-      <div class="experience-card">
+      <div class="experience-card" v-for="item in data.experience">
         <div class="card-title">
           <div class="fl card-left">
             <div class="left-title">体验金</div>
@@ -19,7 +19,7 @@
         </div>
         <div class="card-middle">
           <div class="fl card-left">
-            <div class="left-title">8888元</div>
+            <div class="left-title">{{item.money}}</div>
           </div>
           <div class="fr card-right">
             <div class="right-title">未使用</div>
@@ -27,59 +27,7 @@
         </div>
         <div class="card-bottom">
           <div class="fl card-left">
-            <div class="left-content">有效期: 2018年08月08日-2018年10月08日</div>
-          </div>
-          <div class="fr card-right">
-            <div class="right-content"></div>
-          </div>
-        </div>
-      </div>
-      <div class="experience-card">
-        <div class="card-title">
-          <div class="fl card-left">
-            <div class="left-title">体验金</div>
-          </div>
-          <div class="fr card-right">
-            <div class="right-title">使用范围: 体验标</div>
-          </div>
-        </div>
-        <div class="card-middle">
-          <div class="fl card-left">
-            <div class="left-title">8888元</div>
-          </div>
-          <div class="fr card-right">
-            <div class="right-title">未使用</div>
-          </div>
-        </div>
-        <div class="card-bottom">
-          <div class="fl card-left">
-            <div class="left-content">有效期: 2018年08月08日-2018年10月08日</div>
-          </div>
-          <div class="fr card-right">
-            <div class="right-content"></div>
-          </div>
-        </div>
-      </div>
-      <div class="experience-card">
-        <div class="card-title">
-          <div class="fl card-left">
-            <div class="left-title">体验金</div>
-          </div>
-          <div class="fr card-right">
-            <div class="right-title">使用范围: 体验标</div>
-          </div>
-        </div>
-        <div class="card-middle">
-          <div class="fl card-left">
-            <div class="left-title">8888元</div>
-          </div>
-          <div class="fr card-right">
-            <div class="right-title">未使用</div>
-          </div>
-        </div>
-        <div class="card-bottom">
-          <div class="fl card-left">
-            <div class="left-content">有效期: 2018年08月08日-2018年10月08日</div>
+            <div class="left-content">有效期: {{item.time}}</div>
           </div>
           <div class="fr card-right">
             <div class="right-content"></div>
@@ -89,7 +37,7 @@
     </div>
     <div v-if="list.listType === 2">
       <group :gutter="37"></group>
-      <div class="experience-card">
+      <div class="experience-card" v-for="item in data.experienceUsed">
         <div class="card-title">
           <div class="fl card-left">
             <div class="left-title">体验金</div>
@@ -100,7 +48,7 @@
         </div>
         <div class="card-middle">
           <div class="fl card-left">
-            <div class="left-title">8888元</div>
+            <div class="left-title">{{item.money}}</div>
           </div>
           <div class="fr card-right">
             <div class="right-title">已使用</div>
@@ -108,59 +56,7 @@
         </div>
         <div class="card-bottom">
           <div class="fl card-left">
-            <div class="left-content">有效期: 2018年08月08日-2018年10月08日</div>
-          </div>
-          <div class="fr card-right">
-            <div class="right-content"></div>
-          </div>
-        </div>
-      </div>
-      <div class="experience-card">
-        <div class="card-title">
-          <div class="fl card-left">
-            <div class="left-title">体验金</div>
-          </div>
-          <div class="fr card-right">
-            <div class="right-title">使用范围: 体验标</div>
-          </div>
-        </div>
-        <div class="card-middle">
-          <div class="fl card-left">
-            <div class="left-title">8888元</div>
-          </div>
-          <div class="fr card-right">
-            <div class="right-title">已使用</div>
-          </div>
-        </div>
-        <div class="card-bottom">
-          <div class="fl card-left">
-            <div class="left-content">有效期: 2018年08月08日-2018年10月08日</div>
-          </div>
-          <div class="fr card-right">
-            <div class="right-content"></div>
-          </div>
-        </div>
-      </div>
-      <div class="experience-card">
-        <div class="card-title">
-          <div class="fl card-left">
-            <div class="left-title">体验金</div>
-          </div>
-          <div class="fr card-right">
-            <div class="right-title">使用范围: 体验标</div>
-          </div>
-        </div>
-        <div class="card-middle">
-          <div class="fl card-left">
-            <div class="left-title">8888元</div>
-          </div>
-          <div class="fr card-right">
-            <div class="right-title">已使用</div>
-          </div>
-        </div>
-        <div class="card-bottom">
-          <div class="fl card-left">
-            <div class="left-content">有效期: 2018年08月08日-2018年10月08日</div>
+            <div class="left-content">有效期: {{item.time}}</div>
           </div>
           <div class="fr card-right">
             <div class="right-content"></div>
@@ -170,7 +66,7 @@
     </div>
     <div v-if="list.listType === 3">
       <group :gutter="37"></group>
-      <div class="experience-card experience-card-expired">
+      <div class="experience-card experience-card-expired" v-for="item in data.experienceExpired">
         <div class="card-title">
           <div class="fl card-left">
             <div class="left-title">体验金</div>
@@ -181,7 +77,7 @@
         </div>
         <div class="card-middle">
           <div class="fl card-left">
-            <div class="left-title">8888元</div>
+            <div class="left-title">{{item.money}}</div>
           </div>
           <div class="fr card-right">
             <div class="right-title">已过期</div>
@@ -189,59 +85,7 @@
         </div>
         <div class="card-bottom">
           <div class="fl card-left">
-            <div class="left-content">有效期: 2018年08月08日-2018年10月08日</div>
-          </div>
-          <div class="fr card-right">
-            <div class="right-content"></div>
-          </div>
-        </div>
-      </div>
-      <div class="experience-card experience-card-expired">
-        <div class="card-title">
-          <div class="fl card-left">
-            <div class="left-title">体验金</div>
-          </div>
-          <div class="fr card-right">
-            <div class="right-title">使用范围: 体验标</div>
-          </div>
-        </div>
-        <div class="card-middle">
-          <div class="fl card-left">
-            <div class="left-title">8888元</div>
-          </div>
-          <div class="fr card-right">
-            <div class="right-title">已过期</div>
-          </div>
-        </div>
-        <div class="card-bottom">
-          <div class="fl card-left">
-            <div class="left-content">有效期: 2018年08月08日-2018年10月08日</div>
-          </div>
-          <div class="fr card-right">
-            <div class="right-content"></div>
-          </div>
-        </div>
-      </div>
-      <div class="experience-card experience-card-expired">
-        <div class="card-title">
-          <div class="fl card-left">
-            <div class="left-title">体验金</div>
-          </div>
-          <div class="fr card-right">
-            <div class="right-title">使用范围: 体验标</div>
-          </div>
-        </div>
-        <div class="card-middle">
-          <div class="fl card-left">
-            <div class="left-title">8888元</div>
-          </div>
-          <div class="fr card-right">
-            <div class="right-title">已过期</div>
-          </div>
-        </div>
-        <div class="card-bottom">
-          <div class="fl card-left">
-            <div class="left-content">有效期: 2018年08月08日-2018年10月08日</div>
+            <div class="left-content">有效期: {{item.time}}</div>
           </div>
           <div class="fr card-right">
             <div class="right-content"></div>
@@ -266,6 +110,59 @@
     },
     data () {
       return {
+        data: {
+          experience: [
+            {
+              money: '8888元',
+              time: '2018年08月08日-2018年10月08日',
+              type: '1'
+            },
+            {
+              money: '8888元',
+              time: '2018年08月08日-2018年10月08日',
+              type: '1'
+            },
+            {
+              money: '8888元',
+              time: '2018年08月08日-2018年10月08日',
+              type: '1'
+            }
+          ],
+          experienceUsed: [
+            {
+              money: '8888元',
+              time: '2018年08月08日-2018年10月08日',
+              type: '2'
+            },
+            {
+              money: '8888元',
+              time: '2018年08月08日-2018年10月08日',
+              type: '2'
+            },
+            {
+              money: '8888元',
+              time: '2018年08月08日-2018年10月08日',
+              type: '2'
+            },
+            {
+              money: '8888元',
+              time: '2018年08月08日-2018年10月08日',
+              type: '2'
+            }
+          ],
+          experienceExpired: [
+            {
+              money: '8888元',
+              time: '2018年08月08日-2018年10月08日',
+              type: '3'
+            },
+            {
+              money: '8888元',
+              time: '2018年08月08日-2018年10月08日',
+              type: '3'
+            }
+          ]
+        },
         list: {
           listType: 1
         }
