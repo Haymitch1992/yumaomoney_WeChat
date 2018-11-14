f75217954f05477772f5bbe9809d56f2dd109e59
高天阳 <13683265113@163.com>
2018-11-14 16:00:31 +0800

    帮助中心

--- src/components/user/help/Help.vue
+++ src/components/user/help/Help.vue
@@ -1,25 +1,28 @@
 <template>
   <div>
     <x-header>帮助中心</x-header>
-    <search
-      @result-click="resultClick"
-      @on-change="getResult"
-      :results="results"
-      v-model="value"
-      position="absolute"
-      auto-scroll-to-top
-      top="46px"
-      @on-focus="onFocus"
-      @on-cancel="onCancel"
-      @on-submit="onSubmit"
-      ref="search"></search>
-    <group>
-      <cell :title="item.title" is-link :key="item.id" v-for="item in list" @click.native="goHelpDetail(item.id, item.title)"></cell>
+    <div style="height: 40px">
+      <search
+        @result-click="resultClick"
+        @on-change="getResult"
+        :results="results"
+        v-model="value"
+        position="absolute"
+        auto-scroll-to-top
+        top="46px"
+        @on-focus="onFocus"
+        @on-cancel="onCancel"
+        @on-submit="onSubmit"
+        ref="search"></search>
+    </div>
+    <group >
+      <cell :title="item.title" is-link :key="item.id" v-for="item in data" @click.native="goHelpDetail(item.id, item.title)"></cell>
     </group>
   </div>
 </template>
 
 <script>
+  import _ from 'lodash'
   import { Group, Cell, XHeader, Search } from 'vux'
   export default {
     name: 'Help',
@@ -31,6 +34,7 @@
     },
     data () {
       return {
+        data: [],
         list: [
           {
             id: '75',
@@ -90,22 +94,40 @@
         window.alert('you click the result item: ' + JSON.stringify(item))
       },
       getResult (val) {
-        console.log('on-change', val)
+        var self = this
+        if(val){
+          self.$refs.search.setBlur()
+          var reg = RegExp(val)
+          self.data = _.filter(self.data, function (o) { return o.title.match(reg) })
+        } else {
+          self.data = self.list
+        }
       },
-      onSubmit () {
-        this.$refs.search.setBlur()
-        this.$vux.toast.show({
-          type: 'text',
-          position: 'top',
-          text: 'on submit'
-        })
+      onSubmit (val) {
+        var self = this
+        if(val){
+          self.$refs.search.setBlur()
+          var reg = RegExp(val)
+          self.data = _.filter(self.data, function (o) { return o.title.match(reg) })
+        } else {
+          self.data = self.list
+        }
       },
       onFocus () {
         console.log('on focus')
       },
       onCancel () {
-        console.log('on cancel')
+        var self = this
+        self.data = self.list
+      },
+      init () {
+        var self = this
+        self.data = self.list
       }
+    },
+    created () {
+      var self = this
+      self.init()
     }
   }
 </script>
