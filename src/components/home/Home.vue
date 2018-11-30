97356e9055f36647976404998e32fe69d7147cee
高天阳 <13683265113@163.com>
2018-11-30 12:00:16 +0800

    项目列表 请求方式修改

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -201,7 +201,7 @@
        */
       getList () {
         var self = this
-        self.$http.post(process.env.BASE_API + '/financeJson.do', {params: { 'curPage': self.curPage }})
+        self.$http.get(process.env.BASE_API + '/financeJson.do', {params: { 'curPage': self.curPage }})
           .then(function (res) {
             _.each(res.data, function (v, k) {
               var item = {
