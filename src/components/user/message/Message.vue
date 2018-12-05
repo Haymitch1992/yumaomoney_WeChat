29fefd2d4cf0525a0fbfa7b73c63a55274af4433
高天阳 <13683265113@163.com>
2018-12-05 11:21:53 +0800

    通知中心 弹出详情

--- src/components/user/message/Message.vue
+++ src/components/user/message/Message.vue
@@ -15,13 +15,16 @@
     <div>
       <popup v-model="popupShow" height="100%">
         <div class="popupFull">
-          <group>
-            <cell :title="item.title" :value="item.time"></cell>
-            <cell-form-preview :list="list"></cell-form-preview>
-            <div class="messageDetail" v-html="item.data.mailContent">
-              {{item.data.mailContent}}
+          <x-header :left-options="{showBack: false}">>信息详情</x-header>
+          <div class="p10">
+            <group>
+              <cell :title="item.title" :value="item.time"></cell>
+              <cell-form-preview :list="list"></cell-form-preview>
+              <div class="messageDetail" v-html="item.data.mailContent">
+                {{item.data.mailContent}}
             </div>
-          </group>
+            </group>
+          </div>
           <div class="pt20">
             <div class="submit-box">
               <x-button @click.native="popupShow = false" type="primary">关闭</x-button>
